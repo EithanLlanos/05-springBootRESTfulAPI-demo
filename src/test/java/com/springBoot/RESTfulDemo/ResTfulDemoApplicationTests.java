0acc4f1cce13c2687cadf92f3ef74de4b9c23346
Rubick <eithanllanosb@gmail.com>
2024-12-07 22:34:44 -0500

    Set up simple base files

---
+++ src/test/java/com/springBoot/RESTfulDemo/ResTfulDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.RESTfulDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

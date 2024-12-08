ac7cae3b403df31479ea4de90b04af5bc57f6758
Rubick <eithanllanosb@gmail.com>
2024-12-08 09:21:16 -0500

    define endpoint for "/students" - return a list of students

--- src/main/java/com/springBoot/restFulDemo/rest/StudentRestController.java
+++ src/main/java/com/springBoot/restFulDemo/rest/StudentRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,7 +15,13 @@ public class StudentRestController {
     // define endpoint for "/students" - return a list of students
     @GetMapping("/students")
     public List<Student> getStudents(){
-        return null;
+        List<Student> theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Poornima","Patel"));
+        theStudents.add(new Student("Mario", "Rossi"));
+        theStudents.add(new Student("Mary", "Smith"));
+
+        return theStudents;
     }
 
-}
+ }

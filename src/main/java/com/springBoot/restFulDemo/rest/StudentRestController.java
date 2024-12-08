29e7ee046b52d1abdc545536c8b59d46a99874b4
Rubick <eithanllanosb@gmail.com>
2024-12-08 08:58:10 -0500

    Create StudentRestController class and define getStudents bases

---
+++ src/main/java/com/springBoot/restFulDemo/rest/StudentRestController.java
@@ -0,0 +1,20 @@
+package com.springBoot.restFulDemo.rest;
+
+import com.springBoot.restFulDemo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    // define endpoint for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        return null;
+    }
+
+}
